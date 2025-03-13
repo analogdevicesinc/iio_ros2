789b084f34e726fefe24f6805fa58590fed0b433
Adrian-Stanea <Adrian.Stanea@analog.com>
2025-03-13 15:57:56 +0200

    Added support for loading configuration parameters from YAML file
    Signed-off-by: Adrian-Stanea <Adrian.Stanea@analog.com>

--- launch/dev_adi_iio_launch.py
+++ launch/dev_adi_iio_launch.py
@@ -12,37 +12,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from launch_ros.actions import Node
+import os
 
+from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-
-    enable_service = ExecuteProcess(
-        cmd=[[
-            'ros2 service call ',
-            '/sensor/AttrEnableTopic ',
-            'adi_iio_interfaces/srv/AttrEnableTopic ',
-            '"{attr_path: \'ad5625/voltage1/powerdown_mode\'}"'
-        ]],
-        shell=True
+    config = os.path.join(
+        get_package_share_directory('adi_iio'),
+        'config',
+        'adi_iio.yaml'
     )
 
     adi_iio_node = Node(
         package='adi_iio',
         executable='adi_iio_node',
-        name='sensor',
+        name='adi_iio_node',
         output='screen',
         emulate_tty=True,
-        parameters=[
-                {'uri': 'ip:192.168.2.1'}],
+        parameters=[config],
         arguments=['--ros-args', '--log-level', 'debug']
-
     )
 
     return LaunchDescription([
         adi_iio_node,
-        enable_service
     ])
