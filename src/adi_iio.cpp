f930c906b6ca0a5805ee3ca2da960669d9a95252
Adrian-Stanea <Adrian.Stanea@analog.com>
2026-06-29 13:57:45 +0000

    src/adi_iio(fix): use service-specific QoS in node services
    Signed-off-by: Adrian-Stanea <Adrian.Stanea@analog.com>

--- src/adi_iio.cpp
+++ src/adi_iio.cpp
@@ -23,6 +23,7 @@ int main(int argc, char ** argv)
 
   std::shared_ptr<IIONode> node = std::make_shared<IIONode>();
   rclcpp::executors::MultiThreadedExecutor executor;
+  const auto service_qos = rclcpp::ServicesQoS();
 
   if (!node->initialized()) {
     RCLCPP_FATAL(rclcpp::get_logger("adi_iio_node"), "Node initialization failed.");
@@ -37,7 +38,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::AttrReadString>(
     std::string(node->get_name()) + "/AttrReadString",
     std::bind(&IIONode::attrReadSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_attr_read_string);
 
   auto cb_group_attr_write_string = node->create_callback_group(
@@ -46,7 +47,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::AttrWriteString>(
     std::string(node->get_name()) + "/AttrWriteString",
     std::bind(&IIONode::attrWriteSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_attr_write_string);
 
   auto cb_group_attr_enable_topic = node->create_callback_group(
@@ -55,7 +56,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::AttrEnableTopic>(
     std::string(node->get_name()) + "/AttrEnableTopic",
     std::bind(&IIONode::attrEnableTopicSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_attr_enable_topic);
 
   auto cb_group_attr_disable_topic = node->create_callback_group(
@@ -64,7 +65,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::AttrDisableTopic>(
     std::string(node->get_name()) + "/AttrDisableTopic",
     std::bind(&IIONode::attrDisableTopicSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_attr_disable_topic);
 
   auto cb_group_buffer_read = node->create_callback_group(
@@ -73,7 +74,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::BufferRead>(
     std::string(node->get_name()) + "/BufferRead",
     std::bind(&IIONode::buffReadSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_buffer_read);
 
   auto cb_group_buffer_write = node->create_callback_group(
@@ -82,7 +83,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::BufferWrite>(
     std::string(node->get_name()) + "/BufferWrite",
     std::bind(&IIONode::buffWriteSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_buffer_write);
 
   auto cb_group_buffer_create = node->create_callback_group(
@@ -91,7 +92,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::BufferCreate>(
     std::string(node->get_name()) + "/BufferCreate",
     std::bind(&IIONode::buffCreateSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_buffer_create);
 
   auto cb_group_buffer_destroy = node->create_callback_group(
@@ -100,7 +101,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::BufferDestroy>(
     std::string(node->get_name()) + "/BufferDestroy",
     std::bind(&IIONode::buffDestroySrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_buffer_destroy);
 
   auto cb_group_buffer_refill = node->create_callback_group(
@@ -109,7 +110,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::BufferRefill>(
     std::string(node->get_name()) + "/BufferRefill",
     std::bind(&IIONode::buffRefillSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_buffer_refill);
 
   auto cb_group_buffer_enable_topic = node->create_callback_group(
@@ -118,7 +119,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::BufferEnableTopic>(
     std::string(node->get_name()) + "/BufferEnableTopic",
     std::bind(&IIONode::buffEnableTopicSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_buffer_enable_topic);
 
   auto cb_group_buffer_disable_topic = node->create_callback_group(
@@ -127,7 +128,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::BufferDisableTopic>(
     std::string(node->get_name()) + "/BufferDisableTopic",
     std::bind(&IIONode::buffDisableTopicSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_buffer_disable_topic);
 
   auto cb_group_list_devices = node->create_callback_group(
@@ -136,7 +137,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::ListDevices>(
     std::string(node->get_name()) + "/ListDevices",
     std::bind(&IIONode::listDevicesSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_list_devices);
 
   auto cb_group_list_channels = node->create_callback_group(
@@ -145,7 +146,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::ListChannels>(
     std::string(node->get_name()) + "/ListChannels",
     std::bind(&IIONode::listChannelsSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_list_channels);
 
   auto cb_group_list_attributes = node->create_callback_group(
@@ -154,7 +155,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::ListAttributes>(
     std::string(node->get_name()) + "/ListAttributes",
     std::bind(&IIONode::listAttributesSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_list_attributes);
 
   auto cb_group_scan_context = node->create_callback_group(
@@ -163,7 +164,7 @@ int main(int argc, char ** argv)
     node->create_service<adi_iio::srv::ScanContext>(
     std::string(node->get_name()) + "/ScanContext",
     std::bind(&IIONode::scanContextSrv, node, std::placeholders::_1, std::placeholders::_2),
-    rmw_qos_profile_services_default,
+    service_qos,
     cb_group_scan_context);
 
   RCLCPP_INFO(rclcpp::get_logger("adi_iio_node"), "IIO Node");
